f659871c6619b42456b0fd5119131ca2ffe9d66a
Jon-Pierre Gentil <jgentil@sebistar.net>
2023-06-20 12:53:40 -0500

    Fixed an issue where duplicate tags are created during the route rewrite phase inside cbv.  References issue #192

--- fastapi_restful/cbv.py
+++ fastapi_restful/cbv.py
@@ -111,6 +111,12 @@ def _register_endpoints(router: APIRouter, cls: Type[Any], *urls: str) -> None:
     prefix_length = len(router.prefix)  # Until 'black' would fix an issue which causes PEP8: E203
     for route in cbv_routes:
         router.routes.remove(route)
+        # remove any tags that were previously assigned by router to route
+        for tag in router.tags:
+            try:
+                route.tags.remove(tag)
+            except ValueError:
+                pass
         route.path = route.path[prefix_length:]
         _update_cbv_route_endpoint_signature(cls, route)
         cbv_router.routes.append(route)
