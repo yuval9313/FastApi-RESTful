55aedb4d10cb8d45c2fdcb7039ba784c6d259735
Pavel Zwerschke <pavelzw@gmail.com>
2023-08-13 11:26:53 +0200

    Add proper __version__

--- fastapi_restful/__init__.py
+++ fastapi_restful/__init__.py
@@ -1,6 +1,15 @@
+import importlib.metadata
+import warnings
+
 from .cbv_base import Api, Resource, set_responses, take_init_parameters
 
-__version__ = "0.4.3"
+try:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError as e:
+    warnings.warn(f"Could not determine version of {__name__}", stacklevel=1)
+    warnings.warn(str(e), stacklevel=1)
+    __version__ = "unknown"
+
 
 __all__ = [
     "Api",
